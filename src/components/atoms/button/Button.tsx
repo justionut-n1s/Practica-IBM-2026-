6dd547b76bb5ab6496f299d14a1dde1ca8f1ce55
TheTransylvanianEagle <tudorlasvegas@gmail.com>
2026-07-21 18:19:40 +0300

    Add atoms

---
+++ src/components/atoms/button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './button.css'
+
+type ButtonProps = {
+    children: React.ReactNode;
+    variant: 'subscribe-button' | 'add-button' | 'shop-now-button' | 'filter-button';
+    onClick: () => void;
+}
+
+export default function Button({ children, variant, onClick}: ButtonProps){
+
+    const TagName = 'button';
+
+    return (
+        <TagName className={'button button--${variant}'}>{children}</TagName>
+    );
+
+}
